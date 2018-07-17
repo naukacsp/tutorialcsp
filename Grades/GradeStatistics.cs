02c90eb915a031ce1f0d2afd134cb16c002fc4c5
x <x>
2018-07-17 14:55:12 +0200

    Branching + Switching

--- Grades/GradeStatistics.cs
+++ Grades/GradeStatistics.cs
@@ -17,5 +17,64 @@ namespace Grades
             HighestGrade = 0;
             LowestGrade = float.MaxValue;
         }
+
+        public string Description
+        {
+            get
+            {
+                string result;
+                switch (LetterGrade)
+                {
+                    case "A":
+                        result = "Excelent";
+                        break;
+
+                    case "B":
+                        result = "Good";
+                        break;
+
+                    case "C":
+                        result = "Average";
+                        break;
+
+                    default:
+                        result = "Improve yourself";
+                        break;
+                }
+
+                return result;
+            }
+        }
+
+        public string LetterGrade
+        {
+            get
+            {
+                float roundedAverageGrade = (float)Math.Round(AverageGrade);
+                string result;
+                if (roundedAverageGrade >= 90)
+                {
+                    result = "A";
+                }
+                else if (roundedAverageGrade >= 80)
+                {
+                    result = "B";
+                }
+                else if (roundedAverageGrade >= 70)
+                {
+                    result = "C";
+                }
+                else if (roundedAverageGrade >= 60)
+                {
+                    result = "D";
+                }
+                else
+                {
+                    result = "F";
+                }
+
+                return result;
+            }
+        }
     }
 }
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -11,18 +11,7 @@ namespace Grades
     {
         private static void Main(string[] args)
         {
-            /*
-            SpeechSynthesizer synth = new SpeechSynthesizer();
-            synth.Speak("Hello internee! You are doing quite well! ");
-            */
             GradeBook book = new GradeBook();
-            /* to invoke both methods */
-            book.NameChanged = new NameChangedDelegate(OnNameChanged);
-            book.NameChanged += OnNameChanged;
-            // overwrite code above: book.NameChanged = new NameChangedDelegate(OnNameChanged2);
-            book.Name = "Scott's Grade Book";
-            book.Name = "Grade Book";
-            book.Name = "";
 
             book.AddGrade(91);
             book.AddGrade(89.5f); //as a float
@@ -30,33 +19,20 @@ namespace Grades
 
             GradeStatistics stats = book.ComputeStatistics();
             WriteResult("Average", stats.AverageGrade);
-            WriteResult("Highest:", (int)stats.HighestGrade);
+            WriteResult("Highest", stats.HighestGrade);
             WriteResult("Lowest", stats.LowestGrade);
-            /*
-            Console.WriteLine("The average of grades is:" + stats.AverageGrade);
-            Console.WriteLine("The highest grade is:" + stats.HighestGrade);
-            Console.WriteLine("The lowest grades is:" + stats.LowestGrade);
-            */
+            WriteResult(stats.Description, stats.LetterGrade);
+
             Console.ReadKey();
-            /*
-            GradeBook book2 = book;
-            book2.AddGrade(75);
-            */
         }
 
-        private static void OnNameChanged(object sender, NameChangedEventArgs args)
+        private static void WriteResult(string description, string result)
         {
-            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
+            Console.WriteLine("{0}: {1:F2}", description, result);
         }
 
         private static void WriteResult(string description, float result)
         {
-            Console.WriteLine(description + ": " + result);
-        }
-
-        private static void WriteResult(string description, int result)
-        {
-            Console.WriteLine("{0}: {1:F2}", description, result);
             Console.WriteLine($"{description}: {result:F2}");
         }
     }
